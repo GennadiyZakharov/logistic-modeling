4a04dc9df6df1af41d5e5fc929d158b57d66f53e
Gennadiy.Zakharov <Gennadiy.Zakharov@d95f355b-d359-7f4e-84be-b0d8bfc3037a>
2011-01-25 20:15:10 +0000

    Some trivial QT code

---
+++ trunk/src/lgcore/signals.py
@@ -0,0 +1,17 @@
+'''
+Created on 25.01.2011
+
+@author: gena
+'''
+
+from PyQt4.QtCore import SIGNAL
+
+# Standard QT signals
+signalClicked = SIGNAL("clicked()")
+signalValueChanged = SIGNAL("valueChanged(int)")
+signalStateChanged = SIGNAL("stateChanged(int)")
+signalTriggered = SIGNAL("triggered()")
+
+# ==== Custom signals ====
+
+
