5a2bac28a8732ea80c031bc5a358ac40eb7a87da
Gennadiy.Zakharov@gmail.com <Gennadiy.Zakharov@gmail.com@d95f355b-d359-7f4e-84be-b0d8bfc3037a>
2011-04-16 06:34:56 +0000

    Link with arrow

--- src/lgcore/lgscheme.py
+++ src/lgcore/lgscheme.py
@@ -1,10 +1,21 @@
+
 from PyQt4 import QtCore
 from lgcore.signals import signalNextTurn
 
 class LgScheme(QtCore.QObject):
+    '''
+    This is parent class for all lgItems
+    It containes all lgItems, can manage, load and save them
+    '''
     def __init__(self):
         super(LgScheme, self).__init__()
         
     def on_NextTurnPressed(self):
         self.emit(signalNextTurn)
+    
+    def openScheme(self):
+        pass
+    
+    def saveScheme(self):
+        pass
         
