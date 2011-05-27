29cc46850c38305afccc836a0f923401f20e547d
Michael.Kvyatkovskiy@gmail.com <Michael.Kvyatkovskiy@gmail.com@d95f355b-d359-7f4e-84be-b0d8bfc3037a>
2011-05-27 18:30:20 +0000

    network package improved; lgmodel: stubs to interact with network added

--- trunk/src/lgnetwork/lgserver.py
+++ trunk/src/lgnetwork/lgserver.py
@@ -1,29 +1,45 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
+from lgnetwork.lgnetworkcommon import NetworkCommon
 
 class LgServer(object):
-    class ClientThread(Thread):
+    def __init__(self, model):
+        self.playerDict = {}
+        self.model = model
+        
+    class ClientThread(Thread, NetworkCommon):
         def __init__(self, channel, details):
             self.channel = channel
             self.details = details
-            Thread.__init__(self)
-    
-        def run ( self ):
-            print 'Received connection:', self.details [0]
-            for x in xrange(10):
-                print self.channel.recv(1024)
-                self.channel.send('Hello %i' % x)
-            self.channel.close()
-            print 'Closed connection:', self.details [0]        
+            Thread.__init__(self)       
             
+        def run(self):
+            if not self.isFinish:
+                self.send()
+                self.receive()
+            else:
+                self.channel.close()
+                        
     def createGame(self):
-        print socket, AF_INET, SOCK_STREAM
         s = socket(AF_INET, SOCK_STREAM)
         s.bind(('', 9000))
-        s.listen(5)        
-        while True:
+        s.listen(5)  
+        playersSet = False  
+        while not playersSet:
             channel, details = s.accept()
-            self.ClientThread(channel, details).start()
-            
-s = LgServer()
-s.createGame()
+            # TODO: Set player ID to client thread 
+            playerID = None
+            self.playerDict[playerID] = self.ClientThread(channel, details, playerID)
+            if len(self.playerDict) == self.model.playersCount:
+                playersSet = True
+    
+    def runGame(self):
+        while not self.model.isFinished():
+            for id in xrange(len(self.playerDict)):
+                self.playerDict[id].setData(self.model.getData())
+                self.playerDict[id].start()
+                self.model.setData(self.playerDict[id].getData())
+        # Disconnect clients
+        for id in xrange(len(self.playerDict)):
+            self.playerDict[id].setIsFinish(True)
+            self.playerDict[id].start()
